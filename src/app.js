e1eea7735ed3d8317ce380bd23d4825550730a3a
Sajid Ali <sajidali.sk404@gmail.com>
2026-04-25 13:51:57 +0500

    slove cors error

--- src/app.js
+++ src/app.js
@@ -20,8 +20,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-// Handle preflight for all routes
-app.options("(.*)", cors(corsOptions))
+
 
 app.use((req, res, next) => {
   res.setHeader(
